a47a0113e3842acf9cc9c9fd3f4c155034e0080b
Dominic Scheirlinck <dominic@vendhq.com>
2015-05-15 14:36:45 +1200

    Allow specifying the jar file location
    Change-Id: I613375d5525e4f0a03bad559d742d6ea1f756086

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,25 +7,29 @@
 # $run_interval - seconds between runs of jmx queries.  Default: 15
 # $log_level    - level at which to log jmxtrans messages.  Default: 'info'
 # $heap_size    - number of MB to use as max and starting heap size. Default: 512
+# $jar_file     - string path to jmxtrans jar. Default: undef - means will not be included in defaults file
 #
 class jmxtrans(
-    $run_interval = 15,
-    $log_level    = 'info',
-    $heap_size    = 512
-)
-{
-    package { 'jmxtrans':
-        ensure  => 'installed',
-    }
+  $run_interval = 15,
+  $log_level    = 'info',
+  $heap_size    = 512,
+  $jar_file     = undef
+) {
+  package {
+    'jmxtrans':
+      ensure  => 'installed';
+  }
 
-    file { '/etc/default/jmxtrans':
-        content => template('jmxtrans/jmxtrans.default.erb')
-    }
+  file {
+    '/etc/default/jmxtrans':
+      content => template('jmxtrans/jmxtrans.default.erb');
+  }
 
-    service { 'jmxtrans':
-        ensure    => 'running',
-        enable    => true,
-        require   => Package['jmxtrans'],
-        subscribe => File['/etc/default/jmxtrans'],
-    }
+  service {
+    'jmxtrans':
+      ensure    => 'running',
+      enable    => true,
+      require   => Package['jmxtrans'],
+      subscribe => File['/etc/default/jmxtrans'];
+  }
 }
